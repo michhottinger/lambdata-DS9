380539c6e9856a48fa781706d9ceb26ff28145a2
michhottinger <michhottinger@users.noreply.github.com>
2019-12-03 13:49:58 -0800

    added some proof of concept utility functions

--- lambdata_michhottinger/__init__.py
+++ lambdata_michhottinger/__init__.py
@@ -1,6 +1,7 @@
 """lambdata - a collection of data science helper functions"""
 import pandas as pd
 import numpy as np
+from scipy import stats
 
 #sample code
 ones = pd.DataFrame(np.ones(10))
@@ -9,3 +10,20 @@ zeros = pd.DataFrame(np.zeros(50))
 #sample functions
 def increment(x):
     return (x + 1)
+
+def mean(x):
+    return (len(x) / 2)
+
+def confidence_interval(data, confidence=0.95):
+    data = np.array(data)
+    mean = np.mean(data)
+    n = len(data)
+    stderr = stats.sem(data)
+    margin = stderr * stats.t.ppf((1 + confidence) / 2.0, n - 1)
+    return (mean, mean - margin, mean + margin)
+
+def top_perc(df, cols):
+    for col in cols:
+        df = df[(df[col] >= np.percentile(df[col], 0.5)) &
+            (df[col] <= np.percentile(df[col], 99.5))]
+    return df
